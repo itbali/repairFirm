41bfa4d957f95725b46e12c0077feb5e954fdac5
dyuzhev <alex.itbali@gmail.com>
2023-02-21 23:52:07 +0300

    - removed unused images,  - refactored name and phone checks  - added phone mask  - refactored some styles

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// Description: This file contains the main javascript code for the website
+// tokens for telegram bot
+var apiToken = "5958435569:AAEN5TT0vj4SSH6x7MLpopAof9gcsTpmlKU";
+var chatId = "-1001806856376"; // This function is used to expand the images when clicked on
+
 function myFunction(imgs, slide) {
   // Get the expanded image
   var expandImg = document.getElementById("expandedImg ".concat(slide)); // Get the image text
@@ -10,56 +15,141 @@ function myFunction(imgs, slide) {
 
   imgText.innerHTML = imgs.alt; // Show the container element (hidden with CSS)
   // expandImg.parentElement.style.display = "block";
-}
+} // geting elements from html
 
-var apiToken = "5958435569:AAEN5TT0vj4SSH6x7MLpopAof9gcsTpmlKU";
-var chatId = "-1001806856376";
-var btn = document.getElementById("calculatePriceButton");
-btn.addEventListener("click", sendMessage);
-textName = document.getElementById("quiz1-1-input");
-textPhone = document.getElementById("quiz1-2-input");
-textName.addEventListener("input", checker);
-textPhone.addEventListener("input", checker);
 
-function checker() {
+var btn = document.getElementById("calculate-price-button");
+var textName = document.getElementById("quiz1-1-input");
+var textPhone = document.getElementById("quiz1-2-input");
+var errorMessage = document.getElementById("errorMessage"); // initialize error messages
+
+var error = {
+  name: undefined,
+  phone: undefined
+}; // check if fields are not empty
+
+function checkName() {
+  error.name = undefined;
+  errorMessage.style.display = "none";
   this.style.borderRadius = "10px 0";
 
   if (this.value.trim()) {
-    this.style.outlineColor = "blue";
     this.style.border = "1px solid var(--border-color)";
+    error.name = undefined;
     return;
   }
 
   this.style.border = "2px solid red";
-  this.style.outlineColor = "red";
   this.placeholder = "Обязательное поле";
+  error.name = "Обязательное поле";
   return;
-}
+} //check if phone number is correct
+
+
+function checkPhone() {
+  error.phone = undefined;
+  errorMessage.style.display = "none";
+
+  if (textPhone.value.trim().length != 18) {
+    textPhone.style.border = "2px solid red"; // textPhone.style.outlineColor = "red";
+
+    textPhone.placeholder = "Номер телефона некорректен";
+    error.phone = "Номер телефона некорректен";
+    return;
+  }
+
+  textPhone.style.border = "1px solid var(--border-color)"; // textPhone.style.outlineColor = "blue";
+
+  return;
+} // phone mask for russian numbers
+
+
+var prefixNumber = function prefixNumber(str) {
+  if (str === "7") {
+    return "7 (";
+  }
+
+  if (str === "8") {
+    return "8 (";
+  }
+
+  if (str === "9") {
+    return "7 (9";
+  }
+
+  return "7 (";
+}; // ======================================
+
+
+textPhone.addEventListener("input", function () {
+  var value = textPhone.value.replace(/\D+/g, "");
+  var numberLength = 11;
+  var result;
+
+  if (textPhone.value.includes("+8") || textPhone.value[0] === "8") {
+    result = "";
+  } else {
+    result = "+";
+  } //
+
+
+  for (var i = 0; i < value.length && i < numberLength; i++) {
+    switch (i) {
+      case 0:
+        result += prefixNumber(value[i]);
+        continue;
+
+      case 4:
+        result += ") ";
+        break;
+
+      case 7:
+        result += "-";
+        break;
+
+      case 9:
+        result += "-";
+        break;
+
+      default:
+        break;
+    }
+
+    result += value[i];
+  } //
+
+
+  textPhone.value = result;
+}); //script for sending message to telegram
 
 function sendMessage() {
-  var text = "<b>\u041B\u0438\u0434 \u0441 \u0434\u0430\u043D\u043D\u044B\u043C\u0438 </b> %0A";
+  var text = "<b>\u041B\u0438\u0434 \u0441 \u0434\u0430\u043D\u043D\u044B\u043C\u0438 </b> %0A"; // check if fields are not empty
 
   if (!textName.value.trim()) {
-    textName.placeholder = "ИМЯ ОБЯЗАТЕЛЬНО";
+    textName.placeholder = "введите имя";
     textName.style.border = "2px solid red";
-    return;
-  }
+    error.name = "Имя обязательно";
+  } // check if phone number is correct
+
 
   if (!textPhone.value.trim()) {
-    textPhone.placeholder = "ТЕЛЕФОН ОБЯЗАТЕЛЬНО";
+    textPhone.placeholder = "введите телефон";
     textPhone.style.border = "2px solid red";
-    return;
-  }
+    error.phone = "Телефон обязательно";
+  } // if there are errors, show them
 
-  text += "<b>\u0418\u043C\u044F: </b>".concat(textName.value.trim(), " %0A<b>\u0422\u0435\u043B\u0435\u0444\u043E\u043D: </b>").concat(textPhone.value.trim(), "%0A<b>\u0412\u043E\u043F\u0440\u043E\u04411: </b>").concat(document.getElementById("quiz2input").value.trim(), "%0A<b>\u0412\u043E\u043F\u0440\u043E\u04412: </b>").concat(document.getElementById("quiz3input").value.trim(), "%0A<b>\u0412\u043E\u043F\u0440\u043E\u04413: </b>").concat(document.getElementById("quiz4input").value.trim());
-  console.log({
-    text: text
-  });
+
+  if (error.name || error.phone) {
+    errorMessage.style.display = "block";
+    errorMessage.textContent = "".concat(error.name || "", " ").concat(error.phone || "");
+    return;
+  } // if there are no errors, send message to telegram
+
+
+  text += "<b>\u0418\u043C\u044F: </b>".concat(textName.value.trim(), " %0A<b>\u0422\u0435\u043B\u0435\u0444\u043E\u043D: </b>").concat(textPhone.value.trim(), "%0A%0A<b>\u043D\u0435\u0434\u0432\u0438\u0436\u0438\u043C\u043E\u0441\u0442\u044C: </b>").concat(document.getElementById("quiz2input").value.trim(), "%0A<b>\u041F\u043B\u043E\u0449\u0430\u0434\u044C: </b>").concat(document.getElementById("quiz3input").value.trim(), "%0A<b>\u0422\u0438\u043F \u0440\u0435\u043C\u043E\u043D\u0442\u0430: </b>").concat(document.getElementById("quiz4input").value.trim());
   var urlString = "https://api.telegram.org/bot".concat(apiToken, "/sendMessage?chat_id=").concat(chatId, "&parse_mode=HTML&text=").concat(text);
   btn.disabled = true;
   btn.textContent = "Расчитываем...";
-  console.log("111111111");
-  debugger;
   fetch(urlString).then(function (res) {
     return res.json();
   }).then(function (d) {
@@ -69,10 +159,15 @@ function sendMessage() {
       btn.textContent = "Расчет запрошен, мы с вами свяжемся";
     }
   })["catch"](function (e) {
-    console.log("error");
     btn.textContent = "Что-то пошло не так, уже чиним";
-    return console.warn({
+    console.warn("error", {
       e: e
     });
+    return;
   });
-}
+} // check functions and phone masks are started here
+
+
+textName.addEventListener("input", checkName);
+textPhone.addEventListener("input", checkPhone);
+btn.addEventListener("click", sendMessage);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// Description: This file contains the main javascript code for the website
+
+// tokens for telegram bot
+let apiToken = "5958435569:AAEN5TT0vj4SSH6x7MLpopAof9gcsTpmlKU";
+let chatId = "-1001806856376";
+
+// This function is used to expand the images when clicked on
 function myFunction(imgs, slide) {
     // Get the expanded image
     var expandImg = document.getElementById(`expandedImg ${slide}`);
@@ -11,61 +18,134 @@ function myFunction(imgs, slide) {
     // expandImg.parentElement.style.display = "block";
 }
 
-let apiToken = "5958435569:AAEN5TT0vj4SSH6x7MLpopAof9gcsTpmlKU";
-let chatId = "-1001806856376";
+// geting elements from html
+const btn = document.getElementById("calculate-price-button");
+const textName = document.getElementById("quiz1-1-input");
+const textPhone = document.getElementById("quiz1-2-input");
+const errorMessage = document.getElementById("errorMessage");
 
-const btn = document.getElementById("calculatePriceButton");
-btn.addEventListener("click", sendMessage);
+// initialize error messages
+let error = {
+    name: undefined,
+    phone: undefined,
+};
 
-textName = document.getElementById("quiz1-1-input");
-textPhone = document.getElementById("quiz1-2-input");
-
-textName.addEventListener("input", checker);
-textPhone.addEventListener("input", checker);
-
-function checker() {
+// check if fields are not empty
+function checkName() {
+    error.name = undefined;
+    errorMessage.style.display = "none";
     this.style.borderRadius = "10px 0";
     if (this.value.trim()) {
-        this.style.outlineColor = "blue";
         this.style.border = "1px solid var(--border-color)";
+        error.name = undefined;
         return;
     }
     this.style.border = "2px solid red";
-    this.style.outlineColor = "red";
     this.placeholder = "Обязательное поле";
+    error.name = "Обязательное поле";
     return;
 }
+//check if phone number is correct
+function checkPhone() {
+    error.phone = undefined;
+    errorMessage.style.display = "none";
+    if (textPhone.value.trim().length != 18) {
+        textPhone.style.border = "2px solid red";
+        // textPhone.style.outlineColor = "red";
+        textPhone.placeholder = "Номер телефона некорректен";
+        error.phone = "Номер телефона некорректен";
+        return;
+    }
+    textPhone.style.border = "1px solid var(--border-color)";
+    // textPhone.style.outlineColor = "blue";
+    return;
+}
+// phone mask for russian numbers
+const prefixNumber = (str) => {
+    if (str === "7") {
+        return "7 (";
+    }
+    if (str === "8") {
+        return "8 (";
+    }
+    if (str === "9") {
+        return "7 (9";
+    }
+    return "7 (";
+};
 
+// ======================================
+textPhone.addEventListener("input", () => {
+    const value = textPhone.value.replace(/\D+/g, "");
+    const numberLength = 11;
+
+    let result;
+    if (textPhone.value.includes("+8") || textPhone.value[0] === "8") {
+        result = "";
+    } else {
+        result = "+";
+    }
+
+    //
+    for (let i = 0; i < value.length && i < numberLength; i++) {
+        switch (i) {
+            case 0:
+                result += prefixNumber(value[i]);
+                continue;
+            case 4:
+                result += ") ";
+                break;
+            case 7:
+                result += "-";
+                break;
+            case 9:
+                result += "-";
+                break;
+            default:
+                break;
+        }
+        result += value[i];
+    }
+    //
+    textPhone.value = result;
+});
+
+//script for sending message to telegram
 function sendMessage() {
     let text = `<b>Лид с данными </b> %0A`;
-
+    // check if fields are not empty
     if (!textName.value.trim()) {
-        textName.placeholder = "ИМЯ ОБЯЗАТЕЛЬНО";
+        textName.placeholder = "введите имя";
         textName.style.border = "2px solid red";
-        return;
+        error.name = "Имя обязательно";
     }
+    // check if phone number is correct
     if (!textPhone.value.trim()) {
-        textPhone.placeholder = "ТЕЛЕФОН ОБЯЗАТЕЛЬНО";
+        textPhone.placeholder = "введите телефон";
         textPhone.style.border = "2px solid red";
+        error.phone = "Телефон обязательно";
+    }
+    // if there are errors, show them
+    if (error.name || error.phone) {
+        errorMessage.style.display = "block";
+        errorMessage.textContent = `${error.name || ""} ${error.phone || ""}`;
         return;
     }
 
-    text += `<b>Имя: </b>${textName.value.trim()} %0A<b>Телефон: </b>${textPhone.value.trim()}%0A<b>Вопрос1: </b>${document
+    // if there are no errors, send message to telegram
+    text += `<b>Имя: </b>${textName.value.trim()} %0A<b>Телефон: </b>${textPhone.value.trim()}%0A%0A<b>недвижимость: </b>${document
         .getElementById("quiz2input")
-        .value.trim()}%0A<b>Вопрос2: </b>${document
+        .value.trim()}%0A<b>Площадь: </b>${document
         .getElementById("quiz3input")
-        .value.trim()}%0A<b>Вопрос3: </b>${document
+        .value.trim()}%0A<b>Тип ремонта: </b>${document
         .getElementById("quiz4input")
         .value.trim()}`;
-    console.log({ text });
 
     let urlString = `https://api.telegram.org/bot${apiToken}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${text}`;
 
     btn.disabled = true;
     btn.textContent = "Расчитываем...";
 
-    console.log("111111111");
-    debugger;
     fetch(urlString)
         .then((res) => res.json())
         .then((d) => {
@@ -75,8 +155,13 @@ function sendMessage() {
             }
         })
         .catch((e) => {
-            console.log("error");
             btn.textContent = "Что-то пошло не так, уже чиним";
-            return console.warn({ e });
+            console.warn("error", { e });
+            return;
         });
 }
+
+// check functions and phone masks are started here
+textName.addEventListener("input", checkName);
+textPhone.addEventListener("input", checkPhone);
+btn.addEventListener("click", sendMessage);
